d27f2402e77b032a4f23757081db011d2b905552
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-08-05 14:02:23 +0700

    update workflow yml file

--- src/main/java/com/exe201/ilink_model/repository/AccountRepository.java
+++ src/main/java/com/exe201/ilink_model/repository/AccountRepository.java
@@ -2,9 +2,11 @@ package com.exe201.ilink_model.repository;
 
 import com.exe201.ilink_model.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
 }
 
